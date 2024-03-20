23e4993b9f3009ffa8ee810b231431d8bff14e49
Kyle Plummer <kyle.plummer@revature.com>
2024-03-20 14:53:22 -0400

    demo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { adminGuardGuard } from './admin-guard.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'one', component: OneComponent, canActivate: [adminGuardGuard]},
+    { path: 'two', component: TwoComponent},
+];
+
